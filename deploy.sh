f8d3699fb061a71c9a69a33d8aca89fe16aa9813
dongrenjian <dongrenjian@meituan.com>
2020-03-18 23:17:07 +0800

    feat: 部署盖路径

--- deploy.sh
+++ deploy.sh
@@ -5,14 +5,14 @@
 set -e
 
 # 获取服务器的CNAME，可以从浏览器中copy curl链接
-CNAME=`curl 'https://raw.githubusercontent.com/Dailoge/ts-axios-doc/master/CNAME' -H 'Connection: keep-alive' -H 'Pragma: no-cache' -H 'Cache-Control: no-cache' -H 'Upgrade-Insecure-Requests: 1' -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.106 Safari/537.36' -H 'Sec-Fetch-Dest: document' -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9' -H 'Sec-Fetch-Site: none' -H 'Sec-Fetch-Mode: navigate' -H 'Sec-Fetch-User: ?1' -H 'Accept-Language: zh-CN,zh;q=0.9' --compressed`
-echo -e "\033[40;32m CNAME: ${CNAME} \033[0m"
-echo $CNAME > CNAME
+# CNAME=`curl 'https://raw.githubusercontent.com/Dailoge/ts-axios-doc/master/CNAME' -H 'Connection: keep-alive' -H 'Pragma: no-cache' -H 'Cache-Control: no-cache' -H 'Upgrade-Insecure-Requests: 1' -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.106 Safari/537.36' -H 'Sec-Fetch-Dest: document' -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9' -H 'Sec-Fetch-Site: none' -H 'Sec-Fetch-Mode: navigate' -H 'Sec-Fetch-User: ?1' -H 'Accept-Language: zh-CN,zh;q=0.9' --compressed`
+# echo -e "\033[40;32m CNAME: ${CNAME} \033[0m"
+# echo $CNAME > CNAME
 # 生成静态文件
 npm run build
 
 # 拷贝CNAME文件
-cp CNAME dist/CNAME
+# cp CNAME dist/CNAME
 
 # 进入生成的文件夹
 cd dist
